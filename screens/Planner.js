c448e75153a47d8a73f2fa3a1fdbc778704fe33b
Samuel <30960735+slownie@users.noreply.github.com>
2022-05-30 11:16:57 -0400

    moral of the story is that everything is harder than it looks

--- screens/Planner.js
+++ screens/Planner.js
@@ -1,7 +1,11 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import {View,Text,StyleSheet,ScrollView} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export default function Planner() {
+
+    const [monday, setMonday] = useState('')
+
     return (
         <View style={styles.container}>
             <ScrollView contentInsetAdjustmentBehavior="automatic" showsVerticalScrollIndicator={false}>
@@ -12,9 +16,74 @@ export default function Planner() {
                     <Text style={styles.titlesTitle}>Meal Planner</Text>
                 </View>
 
-                {/* Search */}
-                
+                {/* Meals of the Weak */}
+                <View style={styles.mealsWrapper}>
+                    <Text style={styles.mealsTitle}>Monday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
 
+                    <Text style={styles.mealsTitle}>Tuesday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                    <Text style={styles.mealsTitle}>Wednesday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                    <Text style={styles.mealsTitle}>Thursday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                    <Text style={styles.mealsTitle}>Friday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                    <Text style={styles.mealsTitle}>Saturday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                    <Text style={styles.mealsTitle}>Sunday</Text>
+                    <Picker
+                        selectedValue={monday}
+                        mode="dropdown"
+                        style={styles.mealPicker}
+                    >   
+                        <Picker.Item label="Select a meal" value=""/>
+                    </Picker>
+
+                </View>
+
+                
             </ScrollView>
         </View>
     );
@@ -41,5 +110,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#000',
         marginTop: 5,
+    },
+
+    mealsWrapper: {
+        marginTop: 30,
+        paddingHorizontal: 20,
+    },
+
+    mealsTitle: {
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+
+    mealPicker: {
+        marginBottom: 20
     }
 });
--- screens/Recipes.js
+++ screens/Recipes.js
@@ -1,17 +1,27 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import {View,Text,StyleSheet,ScrollView,TouchableOpacity,TextInput,Keyboard,KeyboardAvoidingView} from 'react-native';
 
 import RecipeData from '../components/RecipeData';
 
 export default function Recipes() {
 
-    const [recipe, setRecipe] = useState();
-    const [recipeItems, setRecipeItems] = useState([]);
+    const [recipeName, setRecipe] = useState('');
+    const [recipeItems, setRecipeItems] = useState(RecipeData);
 
-    const addRecipe = () => {
+    const addRecipe = (text) => {
         Keyboard.dismiss();
-        setRecipeItems([...recipeItems, recipe]);
-        setRecipe(null);
+        // setRecipeItems([...recipeItems, {id: (recipeItems.length+1).toString(), title: text}]);
+        // setRecipe(null);
+        const newRecipe = {id: (recipeItems.length + 1).toString(), title: text}
+        console.log(newRecipe);
+        var newArray = [...recipeItems, newRecipe];
+        setRecipeItems(newArray);
+    }
+
+    const deleteRecipe = (index) => {
+        let recipeCopy = [...recipeItems];
+        recipeCopy.splice(index, 1);
+        setRecipeItems(recipeCopy);
     }
 
     return (
@@ -28,19 +38,40 @@ export default function Recipes() {
                     behavior={Platform.OS == "ios" ? "padding": "height"}
                     style={styles.writeTaskWrapper}>
 
+                    {/* Write a Recipe Input Field */}
                     <TextInput 
                         style={styles.input}
                         placeholder={'Write a recipe'}
+                        value={recipeName}
+                        onChangeText={recipeName => setRecipe(recipeName)}
                     />
 
-                    <TouchableOpacity>
+                    {/* Add Button */}
+                    <TouchableOpacity onPress={() => addRecipe(recipeName)}>
                         <View style={styles.addWrapper}>
                             <Text>+</Text>
                         </View>
                     </TouchableOpacity>
                 </KeyboardAvoidingView>
 
+                {/* Recipe List */}
+                <View style = {styles.recipeList}>
+                    <Text style={styles.recipeTitle}>Recipes</Text>
 
+                    {/* Recipe Items */}
+                    {
+                        recipeItems.map((item, index) => {
+                                return (
+                                    <TouchableOpacity key={item.id} onPress={() => deleteRecipe(index)}>
+                                        <View key={item.id} style={styles.recipeCardWrapper}>
+                                            <Text>{item.title}</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                );
+                            }
+                        )
+                    }
+                </View>
             </ScrollView>
         </View>
     );
@@ -96,5 +127,26 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderColor: '#C0C0C0',
         borderWidth: 1,
-    }
+    },
+
+    recipeList: {
+        paddingHorizontal: 20,
+    },
+
+    recipeTitle: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginTop: 30,
+    },
+
+    recipeCardWrapper: {
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        paddingTop: 20,
+        paddingLeft: 20,
+        flexDirection: 'row',
+        overflow: 'hidden',
+        marginTop: 10,
+        marginBottom: 20,
+    },
 });
